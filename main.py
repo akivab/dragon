cdae02fe243a25d272acd3f13af80bc9b34f24dc
Akiva Bamberger <akiva.bamberger@gmail.com>
2013-05-06 00:59:51 -0700

    Adding in Markdown, emoticons

--- main.py
+++ main.py
@@ -45,26 +45,26 @@ class Posts(ndb.Model):
     return re.sub('\s+','-', re.sub('[^a-z0-9 ]','',x.title.lower()))
   @staticmethod
   def get_small_date(x):
-    return str(x.created)[:10].replace('-', '')
+    return x.created.strftime("%Y%m%d")
   @property
   def nice_date(x):
     months = 'January,February,March,April,May,June,July,August,September,October,November,December'.split(',')
     date = Posts.get_small_date(x)
     year,month,day = int(date[:4]),int(date[4:6]),int(date[6:])
     return '%s %d, %d' % (months[month-1], day, year)
+  created = ndb.DateTimeProperty(auto_now_add=True)
   small_name = ndb.ComputedProperty(lambda self: Posts.get_small_name(self))
   small_date = ndb.ComputedProperty(lambda self: Posts.get_small_date(self))
-  created = ndb.DateTimeProperty(auto_now_add=True)
   @classmethod
   def getPostById(cls, pid):
     return Posts.gql("WHERE postid=:1", pid).get()
 
 
-def redirect(req, msg, url):
+def Redirect(req, msg, url):
   tpl = "%s <script>setTimeout(function(){ window.location.href='%s';}, 100);</script>"
   req.response.out.write(tpl % (msg, url))
 
-def markdownText(content):
+def MarkdownText(content):
   return markdown.markdown(content,
       extensions=['tables', 'nl2br', 'fenced_code','codehilite', 'emoticons'],
       extension_configs={'emoticons': [
@@ -72,17 +72,31 @@ def markdownText(content):
         ('FILE_EXTENSION', 'png'),
       ]})
 
+def IsLoggedIn(user):
+  return user and user.nickname().lower() == blogsetup.BLOG_USER_ID.lower()
+
+def TemplateValues(template_values):
+  defaults = {
+    'mainimg': blogsetup.BLOG_IMAGE,
+    'maintitle': blogsetup.BLOG_TITLE,
+    'mainsnippet': blogsetup.BLOG_SNIPPET,
+    'disqus_id': blogsetup.DISQUS_ID,
+    'gae_id': blogsetup.GOOGLE_ANALYTICS_ID,
+    'gae_site': blogsetup.GOOGLE_ANALYTICS_SITE
+  }
+  for i in defaults: template_values[i] = defaults[i]
+  return template_values
+
 class MainHandler(webapp.RequestHandler):
   def get(self):
     posts = Posts.gql("WHERE visible=:1 order by created DESC", True)
-    user = users.get_current_user()
-    logged_in = user and user.nickname().lower() == blogsetup.BLOG_USER_ID
+    logged_in = IsLoggedIn(users.get_current_user())
     edit_mode = True if self.request.get('edit') and logged_in else False
-    template_values = {
+    template_values = TemplateValues({
       'published': posts,
       'edit_mode': edit_mode,
-      'logged_in': logged_in
-    }
+      'logged_in': logged_in,
+    })
     template = jinja_environment.get_template('home.html')
     self.response.out.write(template.render(template_values))
 
@@ -96,7 +110,7 @@ class AdminHandler(webapp.RequestHandler):
       url = decorator.authorize_url()
     except AccessTokenRefreshError:
       self.redirect('/')
-    if user and user.nickname().lower() != blogsetup.BLOG_USER_ID:
+    if not IsLoggedIn(user):
       logged_in = False
       url = users.create_login_url()
     posts = Posts.gql("order by created DESC")
@@ -108,13 +122,15 @@ class AdminHandler(webapp.RequestHandler):
     if logged_in:
       files = UpdateHandler.listFiles(self, decorator.http())
       if files:
-        newfiles = [f for f in files if f['id'] not in postids or postids[f['id']] != f['modifiedDate']]
-    template_values = {
+        newfiles = [f for f in files if (f['id'] not in postids
+            or postids[f['id']] != f['modifiedDate'])]
+    template_values = TemplateValues({
       'logged_in': logged_in,
       'url': url,
       'published': published,
       'unpublished': unpublished,
-      'toupdate': newfiles}
+      'toupdate': newfiles
+      })
     template = jinja_environment.get_template('admin.html')
     self.response.out.write(template.render(template_values))
 
@@ -140,7 +156,7 @@ class UpdateHandler(webapp.RequestHandler):
       if fileId:
         self.updateFile(fileId, http)
         return
-      redirect(self, 'Nothing to see here.', '/')
+      Redirect(self, 'Nothing to see here.', '/')
     except AccessTokenRefreshError:
       self.redirect('/')
 
@@ -168,7 +184,7 @@ class UpdateHandler(webapp.RequestHandler):
     title = item['title']
     lastmodified = item['modifiedDate']
     if post and post.lastmodified == lastmodified and not forced:
-      redirect(self, 'No modification needed!', '/get?id=%s' % fileId)
+      Redirect(self, 'No modification needed!', '/get?id=%s' % fileId)
       return
     exportLinks = item['exportLinks']
     htmlResp, htmlContent = http.request(exportLinks['text/html'])
@@ -180,9 +196,9 @@ class UpdateHandler(webapp.RequestHandler):
     htmlContent = unicode(htmlContent, errors='ignore')
     textContent = unicode(textContent, errors='ignore')
     content = contentparser.analyze(textContent, htmlContent)
-    text = markdownText(content)
+    text = MarkdownText(content)
     snippet = ''
-    pic = '/r/defaults/%d.jpg' % randint(1,4)
+    pic = '/r/defaults/%d.jpg' % randint(1,3)
     if post:
       post.text = text
       post.lastmodified = lastmodified
@@ -194,7 +210,7 @@ class UpdateHandler(webapp.RequestHandler):
           lastmodified=lastmodified,
           postid=fileId)
     post.put()
-    redirect(self, 'Loading page.', '/get?id=%s' % fileId)
+    Redirect(self, 'Loading page.', '/get?id=%s' % fileId)
 
 class BlogHandler(webapp.RequestHandler):
   def findPost(self):
@@ -208,7 +224,8 @@ class BlogHandler(webapp.RequestHandler):
       sdate = match.group(1)
       stitle = match.group(2)
       if sdate and stitle:
-        return Posts.query(Posts.small_date==sdate,Posts.small_name==stitle).get()
+        return Posts.query(Posts.small_date==sdate,
+            Posts.small_name==stitle).get()
     return None
   def get(self, *args):
     if re.match('/post', self.request.path):
@@ -219,22 +236,24 @@ class BlogHandler(webapp.RequestHandler):
     else:
       fileId = self.request.get('id')
       post = Posts.getPostById(fileId)
-    user = users.get_current_user()
+    if not post:
+      self.error(404)
+      return
     edit_mode = True if self.request.get('edit') else False
     can_show = True if post and post.visible else False
     has_code = True if (post and post.code and len(post.code) > 0 and
         post.code == self.request.get('code')) else False
     is_main_link = True if post.title.lower() in ('resume', 'about') else False
-    logged_in = user and user.nickname().lower() == blogsetup.BLOG_USER_ID
+    logged_in = IsLoggedIn(users.get_current_user())
     if not can_show and not logged_in and not is_main_link and not has_code:
       self.error(404)
       return
-    template_values = {
+    template_values = TemplateValues({
       'post': post,
       'edit_mode' :edit_mode,
       'logged_in': logged_in,
       'is_main_link': is_main_link
-    }
+    })
     template = jinja_environment.get_template('post.html')
     self.response.out.write(template.render(template_values))
     return
@@ -248,20 +267,23 @@ class BlogHandler(webapp.RequestHandler):
       editMode = True
     elif self.request.path == '/publish':
       visible = True
+      created = datetime.datetime.now()
     user = users.get_current_user()
     post = Posts.getPostById(fileId)
-    if not post or not (user and user.nickname().lower() == blogsetup.BLOG_USER_ID):
+    logging.info('publishing %s %s' % (fileId, post.title))
+    if not post or not IsLoggedIn(user):
       self.error(404)
       return
     if visible is not None: post.visible = visible 
     if code: post.code = code 
     if pic: post.pic = pic 
+    if created: post.created = created
     if snippet: post.snippet = snippet 
     post.put()
     if not editMode:
       self.response.out.write('Published post.')
     else:
-      redirect(self, 'Edited post.', '/')
+      Redirect(self, 'Edited post.', '/')
 
 def main():
   application = webapp.WSGIApplication(
